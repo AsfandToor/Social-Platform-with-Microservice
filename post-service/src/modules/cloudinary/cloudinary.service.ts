1ce2852e1ca0b8eee93374830f9ede1849e587f9
AsfandToor <toorasfand@gmail.com>
2023-09-02 14:39:44 +0500

    Post Service

---
+++ post-service/src/modules/cloudinary/cloudinary.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import {
+  v2 as cloudinary,
+  UploadApiResponse,
+  UploadApiErrorResponse,
+} from 'cloudinary';
+import streamifier from 'streamifier';
+
+type CloudinaryResponse = UploadApiResponse | UploadApiErrorResponse;
+
+@Injectable()
+export class CloudinaryService {
+  uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
+    return new Promise<CloudinaryResponse>((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
+
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
+    });
+  }
+}
---
+++ post-service/src/schemas/comment.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+import * as mongoosePaginate from 'mongoose-paginate-v2';
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+@Schema()
+export class Comment {
+  @Prop()
+  comment: string;
+
+  @Prop({
+    ref: 'User',
+  })
+  author: Types.ObjectId;
+
+  @Prop({
+    ref: 'Post',
+  })
+  post: Types.ObjectId;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
+CommentSchema.plugin(mongoosePaginate);
