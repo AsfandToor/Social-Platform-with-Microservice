c93d3520fa0142bdbfffee07652bed6372df17f6
Abdullah Shakeel <abdullahshakeel599@gmail.com>
2023-09-05 11:23:09 +0500

    added create post functionality

--- client/src/app/home/components/createPost.tsx
+++ client/src/app/home/components/createPost.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
-import { IoMdClose } from 'react-icons/io'
-import { useAtom } from 'jotai'
-import { Seen } from '@/app/atoms/atoms'
+import React from "react";
+import { IoMdClose } from "react-icons/io";
+import { useAtom } from "jotai";
+import { Seen } from "@/app/atoms/atoms";
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Create_Post_Mutation } from "@/app/graphql/mutations/post";
+import apolloClient from "@/app/graphql/client";
 
 const CreatePost = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const handleImageChange = (e: {
+    target: { files: (Blob | MediaSource)[] };
+  }) => {
+    if (e.target.files[0]) {
+      setImageUrl(URL.createObjectURL(e.target.files[0]));
+      console.log(imageUrl);
+    }
+  };
+
+  
+  
+const handleCreatePost = () => {
+  
+
+  // const [createPost] = useMutation(CREATE_POST_MUTATION, {
+  //   client: apolloClient,
+  //   onCompleted: (data) => {
+  //     console.log(data);
+  //   }
+  // });
+  //  createPost({
+  //       variables: {
+  //         createPostInput: {
+  //           caption: 'Your caption here',
+  //           author: 'Author name',
+  //           imageData: "base64String", // Include the base64-encoded image data
+  //         },
+  //       },
+  //     })
+  //       .then((response) => {
+  //         console.log('Post created successfully:', response);
+  //         // Handle success or update UI as needed
+  //       })
+  //       .catch((error) => {
+  //         console.error('Error creating post:', error);
+  //         // Handle error or display an error message
+  //       });
+    
+  };
+
+
+
+
   const [seen, setSeen] = useAtom(Seen);
   const togglePop = () => {
     setSeen(!seen);
@@ -19,10 +67,10 @@ const CreatePost = () => {
           </div>
         </div>
 
-        <div className="flex items-center justify-center w-full h-[700px] flex-col p-5 bg-black">
+        <div className="  flex items-center justify-center w-full h-[700px] flex-col p-5 bg-black">
           <label
             htmlFor="dropzone-file"
-            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer dark:hover:bg-[#fefefe]  hover:bg-[#272727] h-[600px] m-5 bg-black"
+            className="flex flex-col items-center justify-center w-full  border-2 border-gray-300 border-dashed rounded-lg cursor-pointer dark:hover:bg-[#fefefe]  hover:bg-[#272727] h-[600px] m-5 bg-black"
           >
             <div className="flex flex-col items-center justify-center pt-5 pb-6 ">
               <svg
@@ -33,9 +81,9 @@ const CreatePost = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                 />
               </svg>
@@ -47,8 +95,31 @@ const CreatePost = () => {
                 SVG, PNG, JPG or GIF (MAX. 800x400px)
               </p>
             </div>
-            <input id="dropzone-file" type="file" className="hidden" />
+            <input
+              id="dropzone-file"
+              type="file"
+              className="hidden"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
           </label>
+          <div
+            className={
+              imageUrl != ""
+                ? "absolute w-[97%] top-[5.25rem] h-[440px] p-5 bg-black flex justify-center"
+                : "hidden"
+            }
+          >
+            <img
+              src={imageUrl}
+              className={
+                imageUrl != ""
+                  ? " w-[470px] h-[350px] object-cover mb-3 bg-black"
+                  : "hidden"
+              }
+            />
+          </div>
+
           <div className="relative my-5 w-full">
             <input
               type="text"
@@ -64,15 +135,21 @@ const CreatePost = () => {
             </label>
           </div>
           <div className="w-full flex justify-between m-5">
-            <div className="p-2 lg:p-5 px-5 lg:px-10 bg-black border border-white rounded-xl text-red-500 " onClick={togglePop}>
+            <div
+              className="p-2 lg:p-5 px-5 lg:px-10 bg-black border border-white rounded-xl text-red-500 "
+              onClick={togglePop}
+            >
               Cancel
             </div>
-            <div className=" p-2 lg:p-5 bg-blue-500 rounded-xl">Create Post</div>
+            <div className=" p-2 lg:p-5 bg-blue-500 rounded-xl" onClick={handleCreatePost}>
+              Create Post
+            </div>
           </div>
         </div>
       </div>
     </div>
-  );
-}
+);
+  
+};
 
-export default CreatePost
+export default CreatePost;
