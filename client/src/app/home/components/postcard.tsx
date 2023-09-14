73c40f0ebd46e170096f1c4d1921dcc0c89e845d
Abdullah Shakeel <abdullahshakeel599@gmail.com>
2023-09-15 04:09:09 +0500

    Added Add Comment section. Bug: website is reloading each time

--- client/src/app/home/components/postcard.tsx
+++ client/src/app/home/components/postcard.tsx
@@ -8,6 +8,7 @@ import CommentBox from "./commentBox";
 import apolloClient from "@/app/graphql/client";
 import { GET_COMMENTS } from "@/app/graphql/query/comment";
 import { useLazyQuery } from "@apollo/client";
+import AddComment from "./addComment";
 
 const Postcard = (props: any) => {
   const [showComment, setShowComments] = useAtom(showComments);
@@ -25,7 +26,7 @@ const Postcard = (props: any) => {
   });
 
   return (
-    <div className="flex flex-col w-full md:w-[470px] mb-[50px] border-t pt-4 border-[#262626] h-fit">
+    <div className="flex flex-col w-full md:w-[470px] mb-[20px] border-t mt-2 pt-1 border-[#262626] h-fit">
       <div className="text-white text-l font-bold py-3">{props.user}</div>
       <img
         className="w-[470px] h-[350px] object-contain mb-3 bg-black border py-3 border-[#262626]"
@@ -41,7 +42,7 @@ const Postcard = (props: any) => {
             //!if comment length is greater than 0, show "View {size of comment array}" else show "No comments yet" also add tailwind classes to underline is user hover over view comments
             props.comments.length > 0 ? (
               <div
-                className=" hover:underline"
+                className=" hover:underline pt-2"
                 onClick={() => {
                   setComments([]);
                   setShowComments(!showComment);
@@ -59,12 +60,15 @@ const Postcard = (props: any) => {
                 View {props.comments.length} comments
               </div>
             ) : (
-              <div className="">No comments yet</div>
+              <div className="pt-2">No comments yet</div>
             )
           }
-          <div className="font-bold">Add a comment</div>
+      
           <CommentBox id={props.id} />
         </div>
+          <div className="w-full ">
+            <AddComment post={props.id} />  
+          </div>
       </div>
     </div>
   );
